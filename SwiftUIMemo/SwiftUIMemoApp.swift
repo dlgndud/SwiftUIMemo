b603074a09b82d64bdfe33b1439287c73a485819
dlgndud <dlgndud@gmail.com>
2023-02-28 21:10:45 +0900

    Initial Commit

---
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIMemoApp.swift
+//  SwiftUIMemo
+//
+//  Created by HOO YOUNG LEE on 2023/02/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
